f1508976ec33c04d5fd4c108684d6647634ca2ce
Felixvl <digitaleservice@gmail.com>
2024-07-17 20:47:16 +0200

    has a relatie met boodschap gemaakt

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2024  esaf
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * rtb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#[macro_use]
+extern crate cstr;
+extern crate cpp;
+#[macro_use]
+extern crate qmetaobject;
+
+use std::env;
+use std::path::PathBuf;
+
+use gettextrs::{bindtextdomain, textdomain};
+use qmetaobject::*;
+
+mod qrc;
+
+#[derive(QObject, Default)]
+struct BoodschappenLijst {
+    base: qt_base_class!(trait QObject),
+    name: qt_property!(QString; NOTIFY name_changed),
+    name_changed: qt_signal!(),
+    boodschap: Boodschap,
+    compute_greetings: qt_method!(
+        fn compute_greetings(&self, verb: String) -> QString {
+            format!("{verb} {}", self.name).into()
+        }
+    ),
+}
+#[derive(QObject, Default)]
+struct Boodschap{
+    prijs: u32,
+    s: qt_base_class!(trait QObject),
+}
+impl Boodschap{
+
+}
+
+fn main() {
+    init_gettext();
+    unsafe {
+        cpp! { {
+            #include <QtCore/QCoreApplication>
+            #include <QtCore/QString>
+        }}
+        cpp! {[]{
+            QCoreApplication::setApplicationName(QStringLiteral("rtb.esaf"));
+        }}
+    }
+    QQuickStyle::set_style("Suru");
+    qrc::load();
+    qml_register_type::<BoodschappenLijst>(cstr!("BoodschappenLijst"), 1, 0, cstr!("BoodschappenLijst"));
+
+    let mut engine = QmlEngine::new();
+    engine.load_file("qrc:/qml/Main.qml".into());
+    engine.exec();
+}
+
+fn init_gettext() {
+    let domain = "rtb.esaf";
+    textdomain(domain).expect("Failed to set gettext domain");
+
+    let mut app_dir_path = env::current_dir().expect("Failed to get the app working directory");
+    if !app_dir_path.is_absolute() {
+        app_dir_path = PathBuf::from("/usr");
+    }
+
+    let path = app_dir_path.join("share/locale");
+
+    bindtextdomain(domain, path.to_str().unwrap()).expect("Failed to bind gettext domain");
+}
